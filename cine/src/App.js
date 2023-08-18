51945ae6c6788553281f20f2b2316ce5c6a5b0a9
Kagiso Monageng <kagiso.monageng@bastiongroup.co.za>
2023-08-18 13:52:13 +0200

    Tailwind and Flowbite Template

--- cine/src/App.js
+++ cine/src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Hero from './components/hero/Hero';
+import Head from './components/head/Head'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          MM reacting.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Head />
+    <Hero />
+    </>
+        
+     
+    
   );
 }
 
